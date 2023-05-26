23d4f9595361f77c3bd51fb8581c53df6713a74a
Topher Brennan <topherbrennan@Tophers-Air.attlocal.net>
2023-05-25 20:24:06 -0700

    Small chance of nasal coda on syllables that start with consonant.

--- pseudo_summerian.rb
+++ pseudo_summerian.rb
@@ -1,6 +1,6 @@
 VOWELS = ['a', 'e', 'i', 'u']
 CONSONANTS = ['', 'b', 'd', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'z']
-
+NASAL_CONSONANTS = ['m', 'n']
 
 10.times do
   name = ''
@@ -8,7 +8,10 @@ CONSONANTS = ['', 'b', 'd', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'z
   # (3+rand(3)).times do
   5.times do
     syllable = [VOWELS.sample, CONSONANTS.sample].join
-    syllable.reverse! if consonant_first
+    if consonant_first
+        syllable.reverse!
+        syllable << NASAL_CONSONANTS.sample if rand(20) == 0
+    end
     name << syllable
     consonant_first = true if syllable.size == 1
     consonant_first = !consonant_first if rand(3) == 0
